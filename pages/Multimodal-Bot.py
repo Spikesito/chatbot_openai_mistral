287d740c50e12e773903a38a46eb3a6fc9dd1571
adrie <adrien.fernandes@edu.univ-fcomte.fr>
2025-04-25 13:05:25 +0200

    analyse-images fonctionnel mais avec du délai. Djibril

--- pages/Multimodal-Bot.py
+++ pages/Multimodal-Bot.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 import shutil
+import base64
 from openai import OpenAI
 
 st.title("🧠 Chatbot Multimodal")
@@ -16,10 +17,17 @@ if "messages" not in st.session_state:
 # Affichage de l'historique
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        if message["role"] == "assistant" and isinstance(message["content"], dict) and message["content"].get("type") == "image":
+        if isinstance(message["content"], dict) and message["content"].get("type") == "image":
             st.image(message["content"]["url"])
         else:
             st.markdown(message["content"])
+
+  # Fonction d'encodage l'image en base64
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
 
 # Fonction principale
 def handle_prompt(prompt):
@@ -56,6 +64,44 @@ def handle_prompt(prompt):
             max_tokens=1024,
         )
         return response.choices[0].message.content
+
+#fontion d'analyse d'image
+def analyse_image(image_path):
+    # Téléchargement de l'image
+    base64_image = encode_image(image_path)
+
+    # Appel à l'API pour l'analyse d'image
+    response = client.responses.create(
+        model="gpt-4.1-nano",
+        input=[
+            {
+                "role": "user",
+                "content": [
+                    { "type": "input_text", "text": "what's in this image?" },
+                    {
+                        "type": "input_image",
+                        "image_url": f"data:image/jpeg;base64,{base64_image}",
+                    },
+                ],
+            }
+        ],
+    )
+
+    return response.output_text
+
+# Exploration des fichiers
+uploaded_image = st.file_uploader("Télécharge une image", type=["jpg", "jpeg", "png"], key="file_uploader")
+
+if uploaded_image:
+
+    path = f"./downloaded_files/images/{uploaded_image.name}"
+    with open(path, 'wb') as f:
+        f.write(uploaded_image.getbuffer())
+
+    result = analyse_image(path)
+    st.session_state.messages.append({"role": "user", "content": {"type": "image", "url":path}})
+
+    st.session_state.messages.append({"role": "assistant", "content": result})
 
 # Interface de chat
 if prompt := st.chat_input("Tape ton message ou une commande comme /image une licorne dans l’espace"):
